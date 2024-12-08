06a16a488e592dc6de6415504cda7e2f10a6e7bc
Frazer Smith <frazer.dev@icloud.com>
2024-12-08 13:06:17 +0000

    build(deps-dev): replace standard with neostandard (#136)

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,54 +1,54 @@
-import { FastifyPluginCallback } from 'fastify';
+import { FastifyPluginCallback } from 'fastify'
 import {
-    ConsumerGlobalConfig,
-    ConsumerTopicConfig,
-    KafkaConsumer,
-    Message,
-    MetadataOptions,
-    Producer,
-    ProducerGlobalConfig,
-    ProducerTopicConfig
-} from 'node-rdkafka';
+  ConsumerGlobalConfig,
+  ConsumerTopicConfig,
+  KafkaConsumer,
+  Message,
+  MetadataOptions,
+  Producer,
+  ProducerGlobalConfig,
+  ProducerTopicConfig
+} from 'node-rdkafka'
 
 declare module 'fastify' {
-    interface FastifyKafkaMessage extends Pick<Message, 'topic' | 'partition' | 'key'> {
-        payload: unknown;
-    }
+  interface FastifyKafkaMessage extends Pick<Message, 'topic' | 'partition' | 'key'> {
+    payload: unknown;
+  }
 
-    interface FastifyKafkaProducer {
-        producer: Producer;
-        push(message: FastifyKafkaMessage): void;
-        stop(done: () => void): void;
-    }
+  interface FastifyKafkaProducer {
+    producer: Producer;
+    push(message: FastifyKafkaMessage): void;
+    stop(done: () => void): void;
+  }
 
-    interface FastifyKafkaConsumer extends Pick<KafkaConsumer, 'consume' | 'subscribe'> {
-        consumer: KafkaConsumer;
-        stop(done: () => void): void;
-    }
+  interface FastifyKafkaConsumer extends Pick<KafkaConsumer, 'consume' | 'subscribe'> {
+    consumer: KafkaConsumer;
+    stop(done: () => void): void;
+  }
 
-    interface Kafka extends Pick<FastifyKafkaProducer, 'push'>, Pick<FastifyKafkaConsumer, 'consume' | 'subscribe'> {
-        producer?: FastifyKafkaProducer;
-        consumer?: FastifyKafkaConsumer;
-    }
-    interface FastifyInstance {
-        kafka: Kafka;
-    }
+  interface Kafka extends Pick<FastifyKafkaProducer, 'push'>, Pick<FastifyKafkaConsumer, 'consume' | 'subscribe'> {
+    producer?: FastifyKafkaProducer;
+    consumer?: FastifyKafkaConsumer;
+  }
+  interface FastifyInstance {
+    kafka: Kafka;
+  }
 }
 
-type FastifyKafka = FastifyPluginCallback<fastifyKafka.FastifyKafkaOptions>;
+type FastifyKafka = FastifyPluginCallback<fastifyKafka.FastifyKafkaOptions>
 
 declare namespace fastifyKafka {
-    export interface FastifyKafkaOptions {
-        producer?: ProducerGlobalConfig;
-        consumer?: ConsumerGlobalConfig;
-        producerTopicConf?: ProducerTopicConfig;
-        consumerTopicConf?: ConsumerTopicConfig;
-        metadataOptions?: MetadataOptions;
-    }
+  export interface FastifyKafkaOptions {
+    producer?: ProducerGlobalConfig;
+    consumer?: ConsumerGlobalConfig;
+    producerTopicConf?: ProducerTopicConfig;
+    consumerTopicConf?: ConsumerTopicConfig;
+    metadataOptions?: MetadataOptions;
+  }
 
   export const fastifyKafka: FastifyKafka
   export { fastifyKafka as default }
 }
 
-declare function fastifyKafka(...params: Parameters<FastifyKafka>): ReturnType<FastifyKafka>
+declare function fastifyKafka (...params: Parameters<FastifyKafka>): ReturnType<FastifyKafka>
 export = fastifyKafka
--- types/index.test-d.ts
+++ types/index.test-d.ts
@@ -1,30 +1,30 @@
-import Fastify, { FastifyKafkaConsumer, FastifyKafkaProducer, Kafka } from 'fastify';
-import { expectAssignable } from 'tsd';
-import fastifyKafka from '..';
+import Fastify, { FastifyKafkaConsumer, FastifyKafkaProducer, Kafka } from 'fastify'
+import { expectAssignable } from 'tsd'
+import fastifyKafka from '..'
 
 const app = Fastify()
 
 app.register(fastifyKafka, {
-    producer: {
-        'metadata.broker.list': '127.0.0.1:9092',
-        dr_cb: true
-    },
-    consumer: {
-        'metadata.broker.list': '127.0.0.1:9092',
-        'group.id': "new-group-1",
-        'fetch.wait.max.ms': 10,
-        'fetch.error.backoff.ms': 50,
-    },
-    consumerTopicConf: {
-        'auto.offset.reset': 'earliest'
-    },
-    producerTopicConf: {
-        "request.timeout.ms": 10
-    },
-    metadataOptions: {
-        timeout: 1000
-    }
-});
+  producer: {
+    'metadata.broker.list': '127.0.0.1:9092',
+    dr_cb: true
+  },
+  consumer: {
+    'metadata.broker.list': '127.0.0.1:9092',
+    'group.id': 'new-group-1',
+    'fetch.wait.max.ms': 10,
+    'fetch.error.backoff.ms': 50,
+  },
+  consumerTopicConf: {
+    'auto.offset.reset': 'earliest'
+  },
+  producerTopicConf: {
+    'request.timeout.ms': 10
+  },
+  metadataOptions: {
+    timeout: 1000
+  }
+})
 
 // Check whether all properties are merged successfully or not
 expectAssignable<Kafka>(app.kafka)
